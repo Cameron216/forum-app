18fbfe10dfad4271416d3b906bd6b280cc4fdcb3
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-08-27 06:54:06 +0100

    Extend Chakra theme to start adding custom settings

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import customTheme from './extendTheme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider resetCSS={true}>
+    <ChakraProvider resetCSS={true} theme={customTheme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
