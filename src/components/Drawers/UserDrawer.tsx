fb65306b26a5ff6432e996d4f378d5ebd90ed682
Cameron Bailey <cameronbailey216@gmail.com>
2022-09-02 16:28:18 +0100

    Formatted files.

--- src/components/Drawers/UserDrawer.tsx
+++ src/components/Drawers/UserDrawer.tsx
@@ -1,9 +1,8 @@
-
 //have conditional rendering if logged in or not
 //when logged in becomes utility sidebar with profile settings etc.
 
-import React from 'react'
-import { useDisclosure, 
+import React from 'react';
+import {
   Drawer,
   DrawerBody,
   DrawerFooter,
@@ -15,58 +14,57 @@ import { useDisclosure,
   Box,
   FormLabel,
   Stack,
-  Input, } from '@chakra-ui/react'
+  Input,
+} from '@chakra-ui/react';
 
-const UserDrawer = ( {isOpen, onClose}: any ) => {
+const UserDrawer = ({ isOpen, onClose }: any) => {
   const firstField = React.useRef() as React.MutableRefObject<HTMLInputElement>;
-  
+
   return (
     <>
-    <Drawer
-      isOpen={isOpen}
-      placement='right'
-      initialFocusRef={firstField}
-      onClose={onClose}
-    >
-      <DrawerOverlay />
-      <DrawerContent bg='grey.400'>
-        <DrawerCloseButton />
-        <DrawerHeader borderBottomWidth='1px'>
-          Log in
-        </DrawerHeader>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        initialFocusRef={firstField}
+        onClose={onClose}
+      >
+        <DrawerOverlay />
+        <DrawerContent bg="grey.400">
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth="1px">Log in</DrawerHeader>
 
-        <DrawerBody>
-          <Stack spacing='24px'>
-            <Box>
-              <FormLabel htmlFor='username'>Username</FormLabel>
-              <Input
-                ref={firstField}
-                id='username'
-                placeholder='Please enter user name'
-              />
-            </Box>
-
-            <Box>
-              <FormLabel htmlFor='password'>Password</FormLabel>
+          <DrawerBody>
+            <Stack spacing="24px">
+              <Box>
+                <FormLabel htmlFor="username">Username</FormLabel>
                 <Input
-                  type='password'
-                  id='password'
-                  placeholder='Enter password'
+                  ref={firstField}
+                  id="username"
+                  placeholder="Please enter user name"
                 />
-            </Box>
-          </Stack>
-        </DrawerBody>
+              </Box>
 
-        <DrawerFooter borderTopWidth='1px'>
-          <Button variant='outline' mr={3} onClick={onClose}>
-            Cancel
-          </Button>
-          <Button>Log in</Button>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  </>
-  )
-}
+              <Box>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="Enter password"
+                />
+              </Box>
+            </Stack>
+          </DrawerBody>
 
-export default UserDrawer
+          <DrawerFooter borderTopWidth="1px">
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button>Log in</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default UserDrawer;
