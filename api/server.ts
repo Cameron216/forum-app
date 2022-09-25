5ae6b7ca9b3caed4fcd734dbb0ef90a23b9aecbd
Daniel Stewardson <84638889+DanielStewardson@users.noreply.github.com>
2022-09-25 07:35:50 +0100

    install CORS

--- api/server.ts
+++ api/server.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+const cors = require('cors');
 
 import { router as postRouter } from './routes/post';
 import Logger from './lib/logger';
@@ -9,6 +10,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/post', postRouter);
